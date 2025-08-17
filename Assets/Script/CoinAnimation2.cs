a8c4df1ec413fd5b5d3d979d6e2729aa3e652971
SarmadZafar01 <sarmadzafar12@gmail.com>
2025-08-17 09:36:09 +0500

    new Commit

---
+++ Assets/Script/CoinAnimation2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class CoinAnimation2 : MonoBehaviour
+{
+
+
+    public GameObject coinPrefab;   
+    public Transform spawnPoint;    
+    public Transform target;        
+    public int coinCount = 10;      
+    public float flyDuration = 0.5f;
+
+    public void CollectCoins()
+    {
+        for (int i = 0; i < coinCount; i++)
+        {
+            int index = i; 
+
+     
+            GameObject coin = Instantiate(coinPrefab, spawnPoint.position, Quaternion.identity, spawnPoint);
+
+            coin.transform.DOMove(target.position, flyDuration)
+                .SetEase(Ease.InQuad)
+                .SetDelay(index * 0.25f) 
+                .OnComplete(() => Destroy(coin));
+        }
+    }
+}
